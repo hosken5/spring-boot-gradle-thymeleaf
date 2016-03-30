bd262768a217c652bf0b184910b180073f714b0e
hongpengfei <hongpengfei@yimei180.com>
2016-03-30 15:09:04 +0800

    1

---
+++ src/main/java/hello/controller/GreetingController.java
@@ -0,0 +1,26 @@
+package hello.controller;
+
+import hello.domain.Greeting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Controller
+public class GreetingController {
+    Logger logger = LoggerFactory.getLogger(GreetingController.class) ;
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+    @RequestMapping("/")
+    @ResponseBody
+    public Greeting greeting(
+            @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        logger.info("==== in greeting ====");
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
